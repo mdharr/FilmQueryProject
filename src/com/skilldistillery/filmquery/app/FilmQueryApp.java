8a4fe55826aa2cec140558da638824fd32167899
Michael Harrington <mdharr7@gmail.com>
2022-12-04 11:20:49 +0800

    nearly finished film query app

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -1,42 +1,393 @@
 package com.skilldistillery.filmquery.app;
 
+import java.sql.SQLException;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import com.skilldistillery.filmquery.database.DatabaseAccessor;
 import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
+import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-  
-  DatabaseAccessor db = new DatabaseAccessorObject();
+	
 
-  public static void main(String[] args) {
-    FilmQueryApp app = new FilmQueryApp();
-    app.test();
-//    app.launch();
-  }
+	DatabaseAccessor db = new DatabaseAccessorObject();
 
-  private void test() {
-    Film film = db.findFilmById(1);
-    System.out.println(film);
-  }
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		FilmQueryApp app = new FilmQueryApp();
+//		app.test();
+		app.launch();
+	}
 
-  private void launch() {
-    Scanner input = new Scanner(System.in);
-    
-    startUserInterface(input);
-    
-    input.close();
-  }
+	private void test() throws SQLException {
+		Film film = db.findFilmById(1);
+		System.out.println(film);
+	}
 
-  private void startUserInterface(Scanner input) {
-    
-  }
-  
-  /*
-   * some method that would have code like: if (!
-   * findFilmsByActorId(someActorId).isEmpty()) {.....} else
-   * {sysout("no films for that unemployed actor");}
-   */
+	private void launch() throws SQLException {
+		Scanner input = new Scanner(System.in);
+
+		startUserInterface(input);
+
+		input.close();
+	}
+
+	private void startUserInterface(Scanner input) throws SQLException {
+		printTitleScreen();
+		// present a menu in a loop with the following 3 options:
+
+		String popcorn = "\uD83C\uDF7F";
+		String welcomeStr = " The one stop shop for moviegoers!" + "\n";
+		
+		for(int i = 0; i < welcomeStr.length(); i++) {
+			System.out.print(welcomeStr.charAt(i) + "");
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println();
+		try {
+			Thread.sleep(500);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}		
+
+		while (true) {
+			String firstLineStr = "+=======================================+\n"; 
+			                        
+			for(int i = 0; i < firstLineStr.length(); i++) {
+
+				System.out.print(firstLineStr.charAt(i) + "");
+				try {
+					Thread.sleep(10);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("|                  Menu                 |");
+			System.out.println("+---------------------------------------+");
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("| 1) Look up by film id                 |");
+			System.out.println("+---------------------------------------+");
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("| 2) Look up by search keyword          |");
+			System.out.println("+---------------------------------------+");
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("| 3) Exit the application               |");
+			System.out.println("+=======================================+");
+			System.out.println();
+			
+			try {
+				Thread.sleep(500);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}	
+
+			String pleaseChooseStr = "Please enter a number from the menu above: ";
+			
+			for(int i = 0; i < pleaseChooseStr.length(); i++) {
+				System.out.print(pleaseChooseStr.charAt(i) + "");
+				try {
+					Thread.sleep(50);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			int userInput = input.nextInt();
+			input.nextLine();
+
+			if (userInput == 1) {
+				System.out.println();
+				String enterFilmIdStr = "Enter the film id you wish to reference: ";
+				for(int i = 0; i < enterFilmIdStr.length(); i++) {
+					System.out.print(enterFilmIdStr.charAt(i) + "");
+					try {
+						Thread.sleep(50);
+					} catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				int userFilmId = input.nextInt();
+				System.out.println();
+				input.nextLine();
+				Film film = db.findFilmById(userFilmId);
+				printFilmParameters(film);
+				continue;
+				
+			} else if (userInput == 2) {
+				System.out.println();
+				String enterKeywordStr = "Enter a keyword to search our catalogue: ";
+				for(int i = 0; i < enterKeywordStr.length(); i++) {
+					System.out.print(enterKeywordStr.charAt(i) + "");
+					try {
+						Thread.sleep(50);
+					} catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				String userSearchTerm = input.nextLine();
+
+				System.out.println();
+				List<Film> films = db.findFilmsBySearchWord(userSearchTerm);
+				printFilmListParameters(films);
+				continue;
+				
+			} else {
+				System.out.println();
+				String exitStr = "Exiting application...";
+				String byeStr = "Goodbye!";
+				
+				for(int i = 0; i < exitStr.length(); i++) {
+					System.out.print(exitStr.charAt(i) + "");
+					try {
+						Thread.sleep(50);
+					} catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println("\n");
+				for(int i = 0; i < byeStr.length(); i++) {
+					System.out.print(byeStr.charAt(i) + "");
+					try {
+						Thread.sleep(50);
+					} catch(InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				return;
+			}
+		}
+
+	}
+	
+	public void printFilmParameters(Film film) throws SQLException {
+		if(film != null) {
+			System.out.println("Title: " + film.getTitle());
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println();
+			System.out.println("Year: " + film.getReleaseYear());
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println();
+			System.out.println("Rating: " + film.getRating());
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println();
+			System.out.println("Language: " + languageIdConverter(film.getLanguageId()));
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println();
+			System.out.println("Description: " + film.getDescription());
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println();
+			System.out.print("Starring: ");
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+			List<Actor> actors = db.findActorsByFilmId(film.getId());
+			int counter = 1;
+
+			for(Actor actor : actors) {
+				String str = counter + ") " + actor.getFirstName() + " " + actor.getLastName() + ", ";
+				if (str.endsWith(", ") && counter == actors.size()) {
+					str = str.substring(0, str.length() - 2) + ".";
+					System.out.print(str);
+				} else {
+				System.out.print(str);
+				}
+				counter++;
+			}
+			} else {
+			String noFilmIdMatch = "No film was found matching that numeric id.";
+			for(int i = 0; i < noFilmIdMatch.length(); i++) {
+				System.out.print(noFilmIdMatch.charAt(i) + "");
+				try {
+					Thread.sleep(50);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		System.out.println();
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+	}
+	
+	public void printFilmListParameters(List<Film> films) throws SQLException {
+		if(films != null) {
+			for(Film film : films) {
+				System.out.println("Title: " + film.getTitle());
+				try {
+					Thread.sleep(5);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println();
+				System.out.println("Year: " + film.getReleaseYear());
+				try {
+					Thread.sleep(5);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println();
+				System.out.println("Rating: " + film.getRating());
+				try {
+					Thread.sleep(5);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println();
+				System.out.println("Language: " + languageIdConverter(film.getLanguageId()));
+				try {
+					Thread.sleep(5);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println();
+				System.out.println("Description: " + film.getDescription());
+				try {
+					Thread.sleep(5);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println();
+				System.out.print("Starring: ");
+				List<Actor> actors = db.findActorsByFilmId(film.getId());
+				int counter = 1;
+
+				for(Actor actor : actors) {
+					String str = counter + ") " + actor.getFirstName() + " " + actor.getLastName() + ", ";
+					if (str.endsWith(", ") && counter == actors.size()) {
+						str = str.substring(0, str.length() - 2) + ".";
+						System.out.print(str);
+						System.out.println();
+					} else {
+					System.out.print(str);
+					}
+					counter++;
+				}
+				System.out.println();
+
+			}
+		} else {
+			String noMatchingResults = "No matching results found.";
+			for(int i = 0; i < noMatchingResults.length(); i++) {
+				System.out.print(noMatchingResults.charAt(i) + "");
+				try {
+					Thread.sleep(50);
+				} catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+	}
+	
+	public String languageIdConverter(int languageId) {
+		String languageName = ""; 
+		
+		if(languageId == 1) {
+			languageName = "English";
+			
+		} else if(languageId == 2) {
+			languageName = "Italian";
+
+		} else if(languageId == 3) {
+			languageName = "Japanese";
+
+		} else if(languageId == 4) {
+			languageName = "Mandarin";
+
+		} else if(languageId == 5) {
+			languageName = "French";
+
+		} else if(languageId == 6) {
+			languageName = "German";
+
+		}
+		return languageName;
+	}
+	
+	public void printTitleScreen() {
+
+		String[] bigScreen = {
+				"",
+				"                                     |",
+				"                          ___________I____________",
+				"                         ( _____________________ ()",
+				"                       _.-'|                    ||",
+				"                   _.-'   ||       WELCOME      ||",
+				"  ______       _.-'       ||                    ||",
+				" |      |_ _.-'           ||       TO           ||",
+				" |      |_|_              ||                    ||",
+				" |______|   `-._          ||       MOVIE        ||",
+				"    /\\          `-._      ||                    ||",
+				"   /  \\             `-._  ||       NIGHT!       ||",
+				"  /    \\                `-.I____________________||",
+				" /      \\                 ------------------------",
+				"/________\\___________________/________________\\______",
+				""
+		};
+		
+		for(String line: bigScreen) {
+			System.out.println(line);
+			try {
+				Thread.sleep(50);
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.skilldistillery.filmquery.entities.Actor;
@@ -13,35 +15,61 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 
 	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=US/Mountain";
 
-//	public DatabaseAccessorObject() throws ClassNotFoundException {
-//		Class.forName("com.mysql.cj.jdbc.Driver");
-//
-//	}
-	
 	@Override
-	public Film findFilmById(int filmId) {
+	public Film findFilmById(int filmId) throws SQLException {
+
 		Film film = null;
-		
+
+		String userName = "student";
+		String pwd = "student";
+
+		String sql = "SELECT * FROM film WHERE film.id = ?";
+
+		Connection conn = DriverManager.getConnection(URL, userName, pwd);
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, filmId);
+
+		ResultSet filmResult = stmt.executeQuery();
+
+		if (filmResult.next()) {
+			film = new Film(); // create the object
+
+			// here is our mapping of query columns to our object fields:
+			film.setId(filmResult.getInt("id"));
+			film.setTitle(filmResult.getString("title"));
+			film.setDescription(filmResult.getString("description"));
+			film.setReleaseYear(filmResult.getInt("release_year"));
+			film.setLanguageId(filmResult.getInt("language_id"));
+			film.setRentalDuration(filmResult.getInt("rental_duration"));
+			film.setRentalRate(filmResult.getDouble("rental_rate"));
+			film.setLength(filmResult.getInt("length"));
+			film.setReplacementCost(filmResult.getDouble("replacement_cost"));
+			film.setRating(filmResult.getString("rating"));
+			film.setSpecialFeatures(filmResult.getString("special_features"));
+
+		}
+		stmt.close();
+		conn.close();
+
 		return film;
 	}
 
 	@Override
-	public Actor findActorById(int actorId) {
-		// TODO Auto-generated method stub
+	public Actor findActorById(int actorId) throws SQLException {
+
 		Actor actor = null;
-		
+
 		String userName = "student";
 		String pwd = "student";
 
 		// ...
-		String sql = "SELECT id, first_name, last_name FROM actor WHERE id = ?";
+		String sql = "SELECT actor.id, actor.first_name, actor.last_name FROM actor WHERE id = ?";
 
 		Connection conn = DriverManager.getConnection(URL, userName, pwd);
 
-		
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setInt(1, actorId);
-		
+
 		System.out.println(stmt);
 
 		ResultSet actorResult = stmt.executeQuery();
@@ -57,13 +85,88 @@ public class DatabaseAccessorObject implements DatabaseAccessor {
 //			actor.setFilms(findFilmsByActorId(actorId)); // An Actor has Films
 		}
 		// ... conn.close();
+		stmt.close();
+		conn.close();
+
 		return actor;
 	}
 
 	@Override
-	public List<Actor> findActorsByFilmId(int filmId) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Actor> findActorsByFilmId(int filmId) throws SQLException {
+
+		List<Actor> actors = new ArrayList<>();
+
+		String userName = "student";
+		String pwd = "student";
+
+		// ...
+		String sql = "SELECT actor.id, actor.first_name, actor.last_name FROM film JOIN film_actor ON film.id = film_actor.film_id JOIN actor ON actor.id = film_actor.actor_id WHERE film.id = ?";
+
+		Connection conn = DriverManager.getConnection(URL, userName, pwd);
+
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, filmId);
+
+		ResultSet actorResult = stmt.executeQuery();
+
+		while (actorResult.next()) {
+			Actor actor = new Actor();
+			actor.setId(actorResult.getInt("id"));
+			actor.setFirstName(actorResult.getString("first_name"));
+			actor.setLastName(actorResult.getString("last_name"));
+			
+			actors.add(actor);
+		}
+
+		stmt.close();
+		conn.close();
+
+		return actors;
+	}
+
+	@Override
+	public List<Film> findFilmsBySearchWord(String searchWord) throws SQLException {
+		List<Film> films = new ArrayList<>();
+		
+		String userName = "student";
+		String pwd = "student";
+
+		String sql = "SELECT film.id, film.title, film.description, film.release_year, film.language_id, film.rental_duration, film.rental_rate, film.length, film.replacement_cost, film.rating, film.special_features FROM film WHERE film.title LIKE ? OR film.description LIKE ?";
+
+		Connection conn = DriverManager.getConnection(URL, userName, pwd);
+
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, "%" + searchWord + "%");
+		stmt.setString(2, "%" + searchWord + "%");
+
+		ResultSet filmResult = stmt.executeQuery();
+
+		while (filmResult.next()) {
+			Film film = new Film();
+			
+			film.setId(filmResult.getInt("film.id"));
+			film.setTitle(filmResult.getString("film.title"));
+			film.setDescription(filmResult.getString("film.description"));
+			film.setReleaseYear(filmResult.getInt("film.release_year"));
+			film.setLanguageId(filmResult.getInt("film.language_id"));
+			film.setRentalDuration(filmResult.getInt("film.rental_duration"));
+			film.setRentalRate(filmResult.getDouble("film.rental_rate"));
+			film.setLength(filmResult.getInt("film.length"));
+			film.setReplacementCost(filmResult.getDouble("film.replacement_cost"));
+			film.setRating(filmResult.getString("film.rating"));
+			film.setSpecialFeatures(filmResult.getString("film.special_features"));
+
+			// Assuming you have a user object
+
+
+			films.add(film);
+
+		}
+
+		stmt.close();
+		conn.close();
+
+		return films;
 	}
 
 }
